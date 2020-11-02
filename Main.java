a4cd52895718478d2e2f32c4a328a62a9a6cfaf3
celer7841 <celer7841@wrdsb.ca>
2020-11-02 13:53:09 +0000

    finished

--- Main.java
+++ Main.java
@@ -5,6 +5,15 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+  JPanel mainPanel;
+
+  JLabel nLabel;
+  JLabel factorialLabel;
+
+  JTextField input;
+  JTextField output;
+
+  JButton calcButton;
   
 
 
@@ -18,7 +27,43 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    // initialize the mainPanel
+    mainPanel = new JPanel();
+
+    mainPanel.setLayout(null);
+
+    nLabel = new JLabel("n");
+    nLabel.setBounds(20, 20, 20, 20);
+    factorialLabel = new JLabel("factorial(n)");
+    factorialLabel.setBounds(120, 20, 80, 20);
+
+    // initialize the input text fields
+    input = new JTextField();
+    output = new JTextField();
+
+    // location and size 
+    input.setBounds(50, 20, 50, 20);
+    output.setBounds(210, 20, 100, 20);
+
+    // initialize the button
+    calcButton = new JButton("CALC");
+    calcButton.setBounds(320, 20, 80, 20);
+
+    calcButton.addActionListener(this);
+
+    calcButton.setActionCommand("CALC");
+
+    mainPanel.add(calcButton);
+
+    mainPanel.add(input);
+    mainPanel.add(output);
+
+    mainPanel.add(nLabel);
+    mainPanel.add(factorialLabel);
+
+    frame.add(mainPanel);
+  
     
 
   }
@@ -27,6 +72,16 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+
+    String inputText = input.getText();
+    int inputInt = Integer.parseInt(inputText);
+    int product = 1;
+
+    // create for loop and declare 
+    for(int mutiply = inputInt; mutiply >= 1; mutiply--){
+
+    } 
 
   }
 
